dc64cbb3e9a993950b3dc285936f79a31c4f74ea
James <j.a.forsyth91@gmail.com>
2023-04-03 15:48:42 +0100

    complete project

--- javascript.js
+++ javascript.js
@@ -1,17 +1,56 @@
-const choices = ['rock', 'paper', 'scissors']
-
-let playerSelection = window.prompt('Select: rock, paper or scissors');
+let choices = ["rock", "paper", "scissors"];
 
 let score = {
     playerScore: 0,
     computerScore: 0,
     tiedGames: 0
+};
+
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+const resultsDiv1 = document.querySelector('#resultsDiv1');
+const resultsDiv2 = document.querySelector('#resultsDiv2');
+const resultsDiv3 = document.querySelector('#resultsDiv3');
+
+function displayScore(){
+    resultsDiv1.textContent = `Player's Score: ${score.playerScore}`;
+    resultsDiv2.textContent = `Computer's Score: ${score.computerScore}`;
+    resultsDiv3.textContent = `Tied Games: ${score.tiedGames}`;
 }
+
+rockButton.addEventListener('click', function(){
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (score.playerScore == 5){
+        resultsDiv1.textContent = `Player's Score: ${score.playerScore}`;
+        resultsDiv2.textContent = `Computer's Score: ${score.computerScore}`;
+        resultsDiv3.textContent = `Player Wins`;
+    } else if (score.computerScore == 5){
+        resultsDiv1.textContent = `Player's Score: ${score.playerScore}`;
+        resultsDiv2.textContent = `Computer's Score: ${score.computerScore}`;
+        resultsDiv3.textContent = `Computer Wins`;
+    } else displayScore();
+});
+
+paperButton.addEventListener('click', function(){
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    displayScore();
+});
+
+scissorsButton.addEventListener('click', function(){
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    displayScore();
+});
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     let choice = choices[randomIndex];
-    console.log(`Computer's choice is ${choice}`);
     return choice;
 }
 
@@ -41,15 +80,12 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-
-    for (let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice()
-        console.log(`Player's selection is ${playerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(score);
-        console.log('');
-    }
-}
-
-game()
+// function game(){
+//     // for (let i = 0; i < 5; i++){
+//         const computerSelection = getComputerChoice()
+//         console.log(`Player's selection is ${playerSelection}`);
+//         console.log(playRound(playerSelection, computerSelection));
+//         console.log(score);
+//         console.log('');
+//     // }
+// }
